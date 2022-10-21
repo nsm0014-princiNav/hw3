0621f950b714ed7a76031135c5a521420ccc8404
Noah Miller <42622436+Noahfireball1@users.noreply.github.com>
2022-10-21 14:43:05 -0500

    working on q5

--- Q5.m
+++ Q5.m
@@ -6,4 +6,6 @@ format shortg
 
 %% Begin Question 5
 
-[r_eb_e] = llh2xyz(27.98777777777778,86.94444444444444, 8850)
+[r_eb_e] = llh2xyz(27.98777777777778,86.94444444444444, 8850);
+
+[phi, lambda, h] = xyz2llh(r_eb_e(1),r_eb_e(2),r_eb_e(3))
